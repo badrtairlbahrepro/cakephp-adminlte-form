9a0bf7a7b6af1f5f72f716ae07fc134bc968ff4e
badr <badr@north-star.network>
2025-10-27 07:18:57 +0100

    Add form prefix to routes to avoid conflicts with home page

--- config/routes.php
+++ config/routes.php
@@ -8,11 +8,13 @@ declare(strict_types=1);
 use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $builder) {
-    $builder->connect('/', ['controller' => 'FormBuilder', 'action' => 'index']);
-    $builder->connect('/contact', ['controller' => 'FormBuilder', 'action' => 'contact']);
-    $builder->connect('/register', ['controller' => 'FormBuilder', 'action' => 'register']);
-    $builder->connect('/profile', ['controller' => 'FormBuilder', 'action' => 'profile']);
-    $builder->connect('/search', ['controller' => 'FormBuilder', 'action' => 'search']);
-    $builder->connect('/multiple', ['controller' => 'FormBuilder', 'action' => 'multiple']);
-    $builder->connect('/switches', ['controller' => 'FormBuilder', 'action' => 'switches']);
+    $builder->prefix('form', function (RouteBuilder $builder) {
+        $builder->connect('/', ['controller' => 'FormBuilder', 'action' => 'index']);
+        $builder->connect('/contact', ['controller' => 'FormBuilder', 'action' => 'contact']);
+        $builder->connect('/register', ['controller' => 'FormBuilder', 'action' => 'register']);
+        $builder->connect('/profile', ['controller' => 'FormBuilder', 'action' => 'profile']);
+        $builder->connect('/search', ['controller' => 'FormBuilder', 'action' => 'search']);
+        $builder->connect('/multiple', ['controller' => 'FormBuilder', 'action' => 'multiple']);
+        $builder->connect('/switches', ['controller' => 'FormBuilder', 'action' => 'switches']);
+    });
 };
