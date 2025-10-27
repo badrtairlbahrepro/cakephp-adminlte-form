7140b942344452c4524a48f534c6e166504728a9
badr <badr@north-star.network>
2025-10-27 07:03:43 +0100

    Add config/routes.php for automatic route loading

---
+++ config/routes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Routes configuration for AdminLteForm plugin
+ */
+
+declare(strict_types=1);
+
+use Cake\Routing\RouteBuilder;
+
+return function (RouteBuilder $builder) {
+    $builder->connect('/', ['controller' => 'FormBuilder', 'action' => 'index']);
+    $builder->connect('/contact', ['controller' => 'FormBuilder', 'action' => 'contact']);
+    $builder->connect('/register', ['controller' => 'FormBuilder', 'action' => 'register']);
+    $builder->connect('/profile', ['controller' => 'FormBuilder', 'action' => 'profile']);
+    $builder->connect('/search', ['controller' => 'FormBuilder', 'action' => 'search']);
+    $builder->connect('/multiple', ['controller' => 'FormBuilder', 'action' => 'multiple']);
+    $builder->connect('/switches', ['controller' => 'FormBuilder', 'action' => 'switches']);
+};
